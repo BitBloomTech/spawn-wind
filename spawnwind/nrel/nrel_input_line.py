764a339273cc3f40eb42ca2069bff90862d936b9
Unknown <philip.bradstock@simmovation.tech>
2019-04-09 17:45:14 -0500

    NREL input line class

---
+++ spawnwind/nrel/nrel_input_line.py
@@ -0,0 +1,54 @@
+import re
+
+
+class NrelInputLine:
+    def __init__(self, line_str):
+        self._line_str = line_str.lstrip()
+        if self._line_str and not (self._line_str.startswith('- ') or self._line_str.startswith('--') or self._line_str[0] == '='):
+            self._value_begin, self._value_end = self._find_value_indices(self._line_str)
+            self._key_begin, self._key_end = self._find_key_indices(self._line_str, self._value_end)
+        else:
+            self._key_begin = None
+            self._value_begin = None
+
+    @staticmethod
+    def _find_value_indices(line_str):
+        match = re.search('^".*?"', line_str)
+        if match:
+            return match.start() + 1, match.end() - 1
+        else:
+            match = re.search('[^\s]+', line_str)
+            return match.start(), match.end()
+
+    @staticmethod
+    def _find_key_indices(line_str, start):
+        match = re.search('[a-zA-Z0-9_]+', line_str[start:])
+        if match is None:
+            raise IOError("Could not find key")
+        return start + match.start(), start + match.end()
+
+    @property
+    def key(self):
+        if self._key_begin is not None:
+            return self._line_str[self._key_begin:self._key_end]
+
+    @property
+    def value(self):
+        if self._value_begin is not None:
+            return self._line_str[self._value_begin:self._value_end]
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            raise TypeError('New value must be string')
+        self._line_str = self._line_str[:self._value_begin] + new_value + self._line_str[self._value_end:]
+        length_increase = len(new_value) - self._value_end + self._value_begin
+        self._value_end += length_increase
+        self._key_begin += length_increase
+        self._key_end += length_increase
+
+    def __bool__(self):
+        return self._key_begin is not None and self._value_begin is not None
+
+    def __str__(self):
+        return self._line_str
---
+++ tests/nrel/nrel_input_line_tests.py
@@ -0,0 +1,52 @@
+import pytest
+from spawnwind.nrel.nrel_input_line import NrelInputLine
+
+
+@pytest.mark.parametrize('line,value,key', [
+    ('4  Ref      - a comment', '4', 'Ref'),
+    ('3.5  Ref      - a comment', '3.5', 'Ref'),
+    ('-14  Ref      - a comment', '-14', 'Ref'),
+    ('Text  Ref     - a comment', 'Text', 'Ref'),
+    ('"a spacey/path" ThePath   - a comment', 'a spacey/path', 'ThePath'),
+    ('      "a spacey/path" ThePath   - a comment', 'a spacey/path', 'ThePath'),
+    ('""  EmptyPath   - a comment', '', 'EmptyPath'),
+    ('---------------', None, None)
+])
+def test_key_and_value_are_correct(line, value, key):
+    input_line = NrelInputLine(line)
+    assert input_line.value == value
+    assert input_line.key == key
+
+
+@pytest.mark.parametrize('line', [
+    '------------',
+    ' ==============',
+    '   ------section name------',
+    '====== section name========',
+])
+def test_line_is_empty(line):
+    input_line = NrelInputLine(line)
+    assert not input_line
+
+
+@pytest.mark.parametrize('before,new_value,after', [
+    ('4  Key', '8', '8  Key'),
+    ('-3.4  Key', '2.4', '2.4  Key'),
+    ('"path/to/file.txt"  Key', 'new/path.txt', '"new/path.txt"  Key'),
+    ('""   Key', 'something', '"something"   Key')
+])
+def test_inserts_value_correctly(before, new_value, after):
+    input_line = NrelInputLine(before)
+    input_line.value = new_value
+    assert after == str(input_line)
+
+
+def test_can_change_value_many_times():
+    line = NrelInputLine('4   Key   - a comment')
+    line.value = '8'
+    assert '8' == line.value
+    line.value = '-4'
+    assert '-4' == line.value
+    line.value = '256'
+    assert '256' == line.value
+    assert 'Key' == line.key
